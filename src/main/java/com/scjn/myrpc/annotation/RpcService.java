f4fc4e8cee6064804ea0d71a67e39afb70a9bff5
scjn <642407967@qq.com>
2020-02-14 20:03:54 +0800

    1

---
+++ src/main/java/com/scjn/myrpc/annotation/RpcService.java
@@ -0,0 +1,9 @@
+package com.scjn.myrpc.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface RpcService {
+    
+}
---
+++ src/test/java/com/scjn/myrpc/MyrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scjn.myrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
